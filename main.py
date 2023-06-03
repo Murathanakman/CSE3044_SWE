877a75567d6ce48135c62ffcac19bccb973a7190
Muhammet Eren ÜNLÜ <muhammeterenunlu@gmail.com>
2023-06-04 02:16:19 +0300

    Added comments

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-#Bu, tüm kodu bir araya getirecek ve çalıştıracak olan dosyadır.
+# This is the file that brings all the code together and runs it.
 from app import app
 
-if __name__ == "__main__": 
+# Checking if this file is the entry point to the program.
+if __name__ == "__main__":
+    # If it is, then start the app with debug mode enabled.
     app.run(debug=True)
+
