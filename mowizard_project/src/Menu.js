0634fc0139a2e5992da4a1cab6ae14f3a0519b61
Musa Meriç <117149250+musamrc@users.noreply.github.com>
2023-06-05 19:07:50 +0300

    Update Menu.js

--- mowizard_project/src/Menu.js
+++ mowizard_project/src/Menu.js
@@ -16,23 +16,116 @@ function Menu() {
   const [selectedOptions, setSelectedOptions] = useState();
 
   // Array of all options
-  const optionList = moviesData.reduce((uniqueOptions, movie) => {
-    const isDuplicate = uniqueOptions.some(
-      (option) => option.value === movie.title
-    );
-    if (!isDuplicate) {
-      uniqueOptions.push({ value: movie.title, label: movie.title });
-    }
-    return uniqueOptions;
-  }, []);
+  const optionList = moviesData.map((movie) => {
+    return { value: movie.title, label: movie.title };
+  });
+
+  const genreList = [
+    { value: "Action", label: "Action" },
+    { value: "Adventure", label: "Adventure" },
+    { value: "Animated", label: "Animated" },
+    { value: "Biography", label: "Biography" },
+    { value: "Comedy", label: "Comedy" },
+    { value: "Crime", label: "Crime" },
+    { value: "Dance", label: "Dance" },
+    { value: "Disaster", label: "Disaster" },
+    { value: "Documentary", label: "Documentary" },
+    { value: "Drama", label: "Drama" },
+    { value: "Erotic", label: "Erotic" },
+    { value: "Family", label: "Family" },
+    { value: "Fantasy", label: "Fantasy" },
+    { value: "Found Footage", label: "Found Footage" },
+    { value: "Historical", label: "Historical" },
+    { value: "Horror", label: "Horror" },
+    { value: "Independent", label: "Independent" },
+    { value: "Legal", label: "Legal" },
+    { value: "Live Action", label: "Live Action" },
+    { value: "Martial Arts", label: "Martial Arts" },
+    { value: "Musical", label: "Musical" },
+    { value: "Mystery", label: "Mystery" },
+    { value: "Noir", label: "Noir" },
+    { value: "Performance", label: "Performance" },
+    { value: "Political", label: "Political" },
+    { value: "Romance", label: "Romance" },
+    { value: "Satire", label: "Satire" },
+    { value: "Science Fiction", label: "Science Fiction" },
+    { value: "Short", label: "Short" },
+    { value: "Silent", label: "Silent" },
+    { value: "Slasher", label: "Slasher" },
+    { value: "Sport", label: "Sport" },
+    { value: "Sports", label: "Sports" },
+    { value: "Spy", label: "Spy" },
+    { value: "Superhero", label: "Superhero" },
+    { value: "Supernatural", label: "Supernatural" },
+    { value: "Suspense", label: "Suspense" },
+    { value: "Teen", label: "Teen" },
+    { value: "Thriller", label: "Thriller" },
+    { value: "War", label: "War" },
+    { value: "Western", label: "Western" },
+  ];
+  const castList = [
+    { value: "Action", label: "Action" },
+    { value: "Adventure", label: "Adventure" },
+    { value: "Animated", label: "Animated" },
+    { value: "Biography", label: "Biography" },
+    { value: "Comedy", label: "Comedy" },
+    { value: "Crime", label: "Crime" },
+    { value: "Dance", label: "Dance" },
+    { value: "Disaster", label: "Disaster" },
+    { value: "Documentary", label: "Documentary" },
+    { value: "Drama", label: "Drama" },
+    { value: "Erotic", label: "Erotic" },
+    { value: "Family", label: "Family" },
+    { value: "Fantasy", label: "Fantasy" },
+    { value: "Found Footage", label: "Found Footage" },
+    { value: "Historical", label: "Historical" },
+    { value: "Horror", label: "Horror" },
+    { value: "Independent", label: "Independent" },
+    { value: "Legal", label: "Legal" },
+    { value: "Live Action", label: "Live Action" },
+    { value: "Martial Arts", label: "Martial Arts" },
+    { value: "Musical", label: "Musical" },
+    { value: "Mystery", label: "Mystery" },
+    { value: "Noir", label: "Noir" },
+    { value: "Performance", label: "Performance" },
+    { value: "Political", label: "Political" },
+    { value: "Romance", label: "Romance" },
+    { value: "Satire", label: "Satire" },
+    { value: "Science Fiction", label: "Science Fiction" },
+    { value: "Short", label: "Short" },
+    { value: "Silent", label: "Silent" },
+    { value: "Slasher", label: "Slasher" },
+    { value: "Sport", label: "Sport" },
+    { value: "Sports", label: "Sports" },
+    { value: "Spy", label: "Spy" },
+    { value: "Superhero", label: "Superhero" },
+    { value: "Supernatural", label: "Supernatural" },
+    { value: "Suspense", label: "Suspense" },
+    { value: "Teen", label: "Teen" },
+    { value: "Thriller", label: "Thriller" },
+    { value: "War", label: "War" },
+    { value: "Western", label: "Western" },
+  ];
 
   // Function triggered on selection
   function handleSelect(data) {
     setSelectedOptions(data);
   }
+
+  //Genre kısmı
+  const [selectedOptionsGenre, setSelectedOptionsGenre] = useState();
+  function handleSelectGenre(data) {
+    setSelectedOptionsGenre(data);
+  }
   const handleNextClick = () => {
     setFormNumber(formNumber + 1);
   };
+
+  //Oyuncular kısmı
+  const [selectedOptionsCast, setSelectedOptionsCast] = useState();
+  function handleSelectCast(data) {
+    setSelectedOptionsCast(data);
+  }
 
   return (
     <div className="app">
@@ -112,13 +205,66 @@ function Menu() {
                 isSearchable={true}
                 isMulti={false}
               />
-              <Button
-                style={{ marginTop: 10, marginLeft: 180 }}
-                variant="outlined"
-                onClick={handleNextClick}
-              >
-                Next
-              </Button>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <p style={{ marginLeft: 3 }}>Boş bırakabilirsiniz.</p>
+                <Button
+                  style={{ marginTop: 10, marginRight: 10 }}
+                  variant="outlined"
+                  onClick={handleNextClick}
+                >
+                  Next
+                </Button>
+              </div>
+            </div>
+          </div>
+        )}
+        {formNumber === 3 && (
+          <div style={{ marginTop: "40%" }} className="form">
+            <h2>Hangi tarz film izlemek istersiniz.</h2>
+            <div style={{ width: 470 }}>
+              <Select
+                options={genreList}
+                placeholder="Film türleri"
+                value={selectedOptionsGenre}
+                onChange={handleSelectGenre}
+                isSearchable={true}
+                isMulti={true}
+              />
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <p style={{ marginLeft: 3 }}>Boş bırakabilirsiniz.</p>
+                <Button
+                  style={{ marginTop: 10, marginRight: 10 }}
+                  variant="outlined"
+                  onClick={handleNextClick}
+                >
+                  Next
+                </Button>
+              </div>
+            </div>
+          </div>
+        )}
+        {formNumber === 4 && (
+          <div style={{ marginTop: "40%" }} className="form">
+            <h2>Hangi tarz oyuncuları izlemek istersiniz?</h2>
+            <div style={{ width: 470 }}>
+              <Select
+                options={castList}
+                placeholder="Film türleri"
+                value={selectedOptionsCast}
+                onChange={handleSelectCast}
+                isSearchable={true}
+                isMulti={true}
+              />
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <p style={{ marginLeft: 3 }}>Boş bırakabilirsiniz.</p>
+                <Button
+                  style={{ marginTop: 10, marginRight: 10 }}
+                  variant="outlined"
+                  onClick={handleNextClick}
+                >
+                  Next
+                </Button>
+              </div>
             </div>
           </div>
         )}
