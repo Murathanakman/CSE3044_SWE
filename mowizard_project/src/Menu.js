d0753a0a802d559a91d411ba0a7936be66c86fd4
Musa Meriç <117149250+musamrc@users.noreply.github.com>
2023-06-05 18:32:35 +0300

    Update Menu.js

--- mowizard_project/src/Menu.js
+++ mowizard_project/src/Menu.js
@@ -6,10 +6,30 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 import Button from "@mui/material/Button";
+import Select from "react-select";
+import moviesData from "../src/movies.json";
 
 function Menu() {
   const [formNumber, setFormNumber] = useState(0);
 
+  // React state to manage selected options
+  const [selectedOptions, setSelectedOptions] = useState();
+
+  // Array of all options
+  const optionList = moviesData.reduce((uniqueOptions, movie) => {
+    const isDuplicate = uniqueOptions.some(
+      (option) => option.value === movie.title
+    );
+    if (!isDuplicate) {
+      uniqueOptions.push({ value: movie.title, label: movie.title });
+    }
+    return uniqueOptions;
+  }, []);
+
+  // Function triggered on selection
+  function handleSelect(data) {
+    setSelectedOptions(data);
+  }
   const handleNextClick = () => {
     setFormNumber(formNumber + 1);
   };
@@ -17,7 +37,10 @@ function Menu() {
   return (
     <div className="app">
       <div>
-        <h1 style={{ marginTop: "20%", fontSize: "50px" }}> Mowizard</h1>
+        <h1 style={{ marginTop: "20%", fontSize: "50px", textAlign: "center" }}>
+          {" "}
+          Mowizard
+        </h1>
         {formNumber === 0 && (
           <div>
             <p>Sizi tanımamız için kısa bir anketi cevaplamanız gerekiyor.</p>
@@ -31,28 +54,40 @@ function Menu() {
           </div>
         )}
         {formNumber === 1 && (
-          <div style={{ marginTop: "100%" }} className="form">
+          <div style={{ marginTop: "40%" }} className="form">
             <FormControl>
-              <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+              <FormLabel id="demo-radio-buttons-group-label">
+                Son kaç yıl içinde yapılmış filmleri izlemek istersiniz.
+              </FormLabel>
               <RadioGroup
                 aria-labelledby="demo-radio-buttons-group-label"
                 defaultValue="female"
                 name="radio-buttons-group"
               >
                 <FormControlLabel
-                  value="female"
+                  value="5"
                   control={<Radio />}
-                  label="Female"
+                  label="Son 5 yıl"
                 />
                 <FormControlLabel
-                  value="male"
+                  value="10"
                   control={<Radio />}
-                  label="Male"
+                  label="Son 10 yıl"
                 />
                 <FormControlLabel
-                  value="other"
+                  value="20"
                   control={<Radio />}
-                  label="Other"
+                  label="Son 20 yıl"
+                />
+                <FormControlLabel
+                  value="50"
+                  control={<Radio />}
+                  label="Son 50 yıl"
+                />
+                <FormControlLabel
+                  value="2023"
+                  control={<Radio />}
+                  label="Farketmez"
                 />
               </RadioGroup>
               <Button
@@ -63,6 +98,28 @@ function Menu() {
                 Next
               </Button>
             </FormControl>
+          </div>
+        )}
+        {formNumber === 2 && (
+          <div style={{ marginTop: "40%" }} className="form">
+            <h2>Favori Filminizi seçiniz.</h2>
+            <div style={{ width: 470 }}>
+              <Select
+                options={optionList}
+                placeholder="Favori filminiz"
+                value={selectedOptions}
+                onChange={handleSelect}
+                isSearchable={true}
+                isMulti={false}
+              />
+              <Button
+                style={{ marginTop: 10, marginLeft: 180 }}
+                variant="outlined"
+                onClick={handleNextClick}
+              >
+                Next
+              </Button>
+            </div>
           </div>
         )}
       </div>
